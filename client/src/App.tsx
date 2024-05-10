819baabe78effc80b823e01027d327456d2972c4
Niyam Bhat <niyambhat@192-168-1-106.tpgi.com.au>
2024-05-11 06:56:45 +0800

    client boiler plate

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { AppContents } from "./AppContents";
+import { UserInfoPage } from "./pages/UserInfoPage";
+
+ const App = () => {
+    return (
+        <div className="page-container">
+      <AppContents/>
+        </div>
+    );
+}
+
+export default App
---
+++ client/src/AppContents.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { EmailVerificationLandingPage } from './pages/EmailVerificationLandingPage';
+import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
+import { LogInPage } from './pages/LogInPage';
+import { SignUpPage } from './pages/SignUpPage';
+import { UserInfoPage } from './pages/UserInfoPage';
+import { PasswordResetLandingPage } from './pages/PasswordResetLandingPage';
+import { PleaseVerifyEmailPage } from './pages/PleaseVerifyEmailPage';
+// import { PrivateRoute } from './auth/PrivateRoute';
+
+export const AppContents = () => {
+    return (
+        <Router>
+            <Routes>
+                {/* <PrivateRoute path="/" exact>
+                    <UserInfoPage />
+                </PrivateRoute> */}
+                <Route path="/" element={   <UserInfoPage />}/>
+                <Route path="/verify-email/:verificationString" element={<EmailVerificationLandingPage />} />
+                <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+                <Route path="/login" element={<LogInPage />} />
+                <Route path="/reset-password/:passwordResetCode" element={<PasswordResetLandingPage />} />
+                <Route path="/please-verify" element={<PleaseVerifyEmailPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+        </Router>
+    );
+}
